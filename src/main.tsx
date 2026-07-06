100effd4d700155869771c2e469460eee807b29f
Nicholas Anakwue <69892313+Nickybert@users.noreply.github.com>
2026-07-06 16:56:16 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import CursorNetwork from "@/components/CursorNetwork";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// FIX: Use a direct relative path instead of the @/ alias
+import CursorNetwork from './components/CursorNetwork.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* The interactive network background */}
     <CursorNetwork />
-    
-    {/* Your main website content */}
     <App />
   </StrictMode>,
 )
