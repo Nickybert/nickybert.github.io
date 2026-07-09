4b00458c0853b5afab1abb336bb04a7804798a4e
Nicholas Anakwue <69892313+Nickybert@users.noreply.github.com>
2026-07-09 12:29:44 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// Background effect
 import CursorNetwork from './components/CursorNetwork'
-// Your new Ko-fi button
+
 import KofiButton from './components/KofiButton' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CursorNetwork />
-    <KofiButton /> {/* Renders the floating button */}
+    <KofiButton /> {/* Put in to Renders my new floating button */}
     <App />
   </StrictMode>,
 )
